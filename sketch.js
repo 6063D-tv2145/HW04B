31b74b085a64f4eb8fc323f8cf753f351673f9c0
Thao Chi Vu <tv2145@nyu.edu>
2023-10-02 13:03:38 -0400

    mouse click function deifnition with randomized elements, radme fill out

--- sketch.js
+++ sketch.js
@@ -1,6 +1,14 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(253, 249, 245)
 }
 
 function draw() {
+}
+
+function mouseClicked() {
+  fill(random(0,255), random(0,255), random(0,255));
+    strokeWeight(random(1,10));
+    stroke(random(0,255), random(0,255), random(0,255));
+    rect(mouseX, mouseY, random(100,200), random(100,200), random(5,20));
 }
